3b7d9538cefb6dfbe2b2fd8e4ceead9ad87cf3b0
alan <alan@DESKTOP-1BFQG99.attlocal.net>
2020-08-31 11:10:40 -0400

    Made a program to do a timer

---
+++ python/timer.py
@@ -0,0 +1,30 @@
+import time, subprocess 
+
+def start_timer_process():
+	print("TIME HAS BEGUN")
+	count = 24
+	while(count != 25):
+		print(f"Min elapsed... {count} mins")
+		time.sleep(60)
+		count = count + 1
+	subprocess.run(["zenity","--width=250", "--height=250", "--warning", "--text='finished time'"])
+	
+	print("Time has completed")
+	retrieve_input()
+
+def retrieve_input():
+	user_input = input("Do you wish to begin program?\n>")
+	if (str(user_input) == "y"):
+		print("Beginning timer for 25 mins")
+		start_timer_process()
+	else:
+		print("Ending program...")
+		exit()
+def main():
+	print("Program designed to time 25 mins for reading/work.")
+	retrieve_input()
+
+
+main()
+
+
