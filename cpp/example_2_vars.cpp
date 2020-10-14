b051f244e8827914121c275b5502c628e1215be5
alan <alan@DESKTOP-1BFQG99.attlocal.net>
2020-10-14 14:26:17 -0400

    Added some C++ examples

---
+++ cpp/example_2_vars.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This is my second example in C++";
+	std::cout << "More practice";
+	std::cout << "New line " << std::endl;
+
+	return 0;
+}
---
+++ cpp/example_3.cpp
@@ -0,0 +1,12 @@
+/*
+ * Alan Caldelas wrote this
+ */
+#include <iostream> 
+using namespace std;
+
+int main() {
+	cout << "*******" << endl;
+	cout << "Alan " << endl;
+	return 0;
+}
+
